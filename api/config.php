49f93f39adc082e9bf24eb6b1190ae18de7a8e26
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2023-05-17 17:37:31 +0200

    feat: preaparation for uploading latex, creating new student

---
+++ api/config.php
@@ -0,0 +1,25 @@
+<?php
+// $hostname = "147.175.98.162:3306";
+$hostname = "localhost:3306";
+$username = "username";
+$password = "password";
+$dbname = "zaverecne";
+$port = "3306";
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+header('Content-Type: application/json');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// require_once('./../../config.php');
+try {
+    //$db = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8", $username, $password);     
+    $db = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
