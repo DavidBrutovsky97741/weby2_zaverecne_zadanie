abfb4200d003065bff11a0e9a4562bb9cf84a71e
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2023-05-18 17:47:36 +0200

    feat: student overview

--- api/students/index.php
+++ api/students/index.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// require('../config.php');
+
 function insertStudentDB($email, $aisId, $name, $surname)
 {
 
@@ -36,5 +38,32 @@ function getStudentByAisId(PDO $db, int $aisId)
     }
 }
 
+function getStudentsSummary(PDO $db)
+{
+    try {
+        $sql = "SELECT 
+        Students.aisId, 
+        Students.name, 
+        Students.surname, 
+        Students.generated_task_sets_count,
+        (SELECT COUNT(*) 
+         FROM Student_task_sets 
+         WHERE Student_task_sets.student_id = Students.id 
+         AND Student_task_sets.state = 'SUBMITED') AS submitted_task_sets_count,
+        (SELECT SUM(points_acquired)
+         FROM Student_task_sets
+         WHERE Student_task_sets.student_id = Students.id
+         AND Student_task_sets.state = 'SUBMITED') AS total_points_acquired
+    FROM Students";
+        $stmt = $db->prepare($sql);
+        if ($stmt->execute()) {
+            $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $response;
+        }
+        return null;
+    } catch (Exception $e) {
+        return null;
+    }
+}
 
 ?>
