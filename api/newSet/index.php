13e0c34c78f6ce3ed7ee07f2003bfe1a9c70045a
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2023-05-18 12:09:22 +0200

    feat: new db

--- api/newSet/index.php
+++ api/newSet/index.php
@@ -12,47 +12,57 @@ $latexText = '\documentclass[a4paper, 12pt]{article}
 \begin{document}
 
 
-\section*{B34A5A}
+\section*{O23A7A}
 \begin{task}
-    Nájdite prenosovú funkciu $F(s)=\dfrac{Y(s)}{W(s)}$ pre systém opísaný blokovou schémou: 
-
-    \includegraphics{zadanie99/images/blokovka01_00002.jpg} 
+    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou
+    \begin{equation*}
+        F(s)=\dfrac{6}{(5s+2)^2}e^{-4s}
+    \end{equation*}
 \end{task} 
 
 \begin{solution}
     \begin{equation*}
-        \dfrac{2s^2+13s+10}{s^3+7s^2+18s+15}
+        y(t)=\left[ \dfrac{3}{2}-\dfrac{3}{2}e^{-\frac{2}{5}(t-4)}-\dfrac{3}{5}(t-4)e^{-\frac{2}{5}(t-4)} \right] \eta(t-4)
     \end{equation*}
 \end{solution}
 
-\section*{BA23B4}
-\begin{task}
-    Nájdite prenosovú funkciu $F(s)=\dfrac{Y(s)}{W(s)}$ pre systém opísaný blokovou schémou: 
+%===============================================================
 
-    \includegraphics{zadanie99/images/blokovka01_00003.jpg} 
+
+\section*{O5A67B}
+\begin{task}
+    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou
+    \begin{equation*}
+        F(s)=\dfrac{35}{(2s+5)^2}e^{-6s}
+    \end{equation*}
 \end{task} 
 
 \begin{solution}
     \begin{equation*}
-        \dfrac{7s+10}{2s^3+11s^2+12s+10}
+        y(t)=\left[ \dfrac{7}{5}-\dfrac{7}{5}e^{-\frac{5}{2}(t-6)}-\dfrac{7}{2}(t-6)e^{-\frac{5}{2}(t-6)} \right] \eta(t-6)
     \end{equation*}
 \end{solution}
 
-\section*{B2A76C}
-\begin{task}
-    Nájdite prenosovú funkciu $F(s)=\dfrac{Y(s)}{W(s)}$ pre systém opísaný blokovou schémou: 
+%=============================================================
 
-    \includegraphics{zadanie99/images/blokovka01_00004.jpg} 
+\section*{OAC346}
+\begin{task}
+    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou
+    \begin{equation*}
+        F(s)=\dfrac{12}{(5s+4)^2}e^{-7s}
+    \end{equation*}
 \end{task} 
 
 \begin{solution}
     \begin{equation*}
-        4\dfrac{3s+1}{s^3+10s^2+13s+14}
+        y(t)=\left[ \dfrac{3}{4}-\dfrac{3}{4}e^{-\frac{4}{5}(t-7)}-\dfrac{3}{5}(t-7)e^{-\frac{4}{5}(t-7)} \right] \eta(t-7)
     \end{equation*}
 \end{solution}
 
 
-\end{document}';
+
+\end{document}
+';
 
 
 ini_set('display_errors', 1);
@@ -81,6 +91,40 @@ if (
     return;
 }
 
+abstract class Result
+{
+    public function isOk(): bool
+    {
+        return $this instanceof Ok;
+    }
+
+    public function isErr(): bool
+    {
+        return $this instanceof Err;
+    }
+}
+
+class Ok extends Result
+{
+    public $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+}
+
+class Err extends Result
+{
+    public $error;
+
+    public function __construct($error)
+    {
+        $this->error = $error;
+    }
+}
+
+
 function parseLaTeX($text)
 {
     $pattern = '/\\\\section\*\{(.+?)\}.*?\\\\begin\{task\}(.*?)\\\\includegraphics\{(.*?)\}.*?\\\\end\{task\}.*?\\\\begin\{solution\}(.*?)\\\\end\{solution\}/s';
@@ -99,16 +143,116 @@ function parseLaTeX($text)
     return $tasks;
 }
 
-function createNewTaskSet(PDO $db, array $request)
+function validateImages($tasks, $images): bool
+{
+    foreach ($tasks as $task) {
+        if (count($task["images"]) === 0)
+            continue;
+        $contains = false;
+        foreach ($images as $image) {
+            if (str_contains($task["image"], $image["fileName"]))
+                $contains = true;
+        }
+        if ($contains == false)
+            return false;
+    }
+    return true;
+
+    // foreach ($images as $image) {
+    //     $contains = false;
+    //     foreach ($tasks as $task) {
+    //         if (str_contains($task["image"], $image["name"]))
+    //             $contains = true;
+    //     }
+    //     if ($contains == false)
+    //         return false;
+    // }
+    // return true;
+}
+
+
+// function findImage($task, $images):array {
+//     foreach ($image as $images) {
+//         if (str_contains($task["image"], $image["fileName"])) return $image
+//     }
+//     return []
+// }
+
+/** */
+function createNewTaskSet(
+    PDO $db
+    , string $name
+    , int $maxPoints
+    , string $latexText
+): Result {
+    try {
+        $sql = "INSERT INTO Tasks_sets (latex_text, max_points, name) VALUES (?, ?, ?)";
+        $stmt = $db->prepare($sql);
+        if ($stmt->execute([$latexText, $maxPoints, $name])) {
+            if ($stmt->rowCount() == 0)
+                return new Err(error: "no rows affected");
+            return new Ok(value: $db->lastInsertId());
+        }
+        return new Err(error: "execute failed");
+    } catch (Exception $e) {
+        return new Err(error: $e->getMessage());
+    }
+}
+
+function createImage(PDO $db, array $image): Result
+{
+    try {
+        $sql = "INSERT INTO Images (image_base64, file_name) VALUES (?, ?)";
+        $stmt = $db->prepare($sql);
+        if ($stmt->execute([$image["image64"], $image["fileName"]])) {
+            if ($stmt->rowCount() == 0)
+                return new Err(error: "no rows affected");
+            return new Ok(value: $db->lastInsertId());
+        }
+        return new Err(error: "execute failed");
+    } catch (Exception $e) {
+        return new Err(error: $e->getMessage());
+    }
+}
+
+// function createTasks(PDO $db, int $taskSetId, array $tasks, array $images){
+//     foreach ($task as $images) {
+//         # code...
+//     }
+// }
+
+function createNewTaskSetBuild(PDO $db, array $request): Result
 {
     $name = $request["name"];
     $maxPoints = $request["maxPoints"];
     $latexText = $request["text"];
     $images = $request["images"];
 
+    $tasks = parseLaTeX($latexText);
+    if (!validateImages($tasks, $images))
+        return new Err(error: "missing images");
+
+    $newSetResult = createNewTaskSet($db, $name, $maxPoints, $latexText);
+    if ($newSetResult->isErr())
+        return $newSetResult;
+    // var_dump(createNewTaskSet($db, $name, $maxPoints, $latexText));
+
+
+
+    // return new Ok(value: );
 }
 
-$tasks = parseLaTeX($latexText);
-var_dump($tasks);
+var_dump(createNewTaskSetBuild($db, [
+    "name" => "aaaas",
+    "maxPoints" => 485,
+    "text" => $latexText,
+    "images" => []
+]))
 
-?>
+    // if ($result->isOk()) {
+//     echo "Result: " . $result->value;
+// } else {
+//     echo "Error: " . $result->error;
+// }
+
+    ?>
--- init.sql
+++ init.sql
@@ -1,11 +1,11 @@
-    -- phpMyAdmin SQL Dump
--- version 5.2.1deb1+jammy2
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 17, 2023 at 07:48 PM
--- Server version: 8.0.33-0ubuntu0.22.04.1
--- PHP Version: 8.2.6
+-- Host: db
+-- Generation Time: May 18, 2023 at 10:08 AM
+-- Server version: 8.0.33
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- CREATE DATABASE zaverecne
+-- Database: `zaverecne`
 --
 
 -- --------------------------------------------------------
@@ -42,7 +42,7 @@ CREATE TABLE `Images` (
   `id` int UNSIGNED NOT NULL,
   `image_base64` longtext NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `task_id` int UNSIGNED NOT NULL
+  `file_name` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -87,7 +87,7 @@ CREATE TABLE `Tasks` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `task_set_id` int UNSIGNED NOT NULL,
   `task_text` text NOT NULL,
-  `task_image_ids` int UNSIGNED NOT NULL,
+  `task_image_id` int UNSIGNED DEFAULT NULL,
   `answer` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -108,6 +108,17 @@ CREATE TABLE `Tasks_sets` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
+-- Dumping data for table `Tasks_sets`
+--
+
+INSERT INTO `Tasks_sets` (`id`, `created_at`, `latex_text`, `max_points`, `start_generate_date`, `end_generate_date`, `name`) VALUES
+(1, '2023-05-18 09:20:35', '\\documentclass[a4paper, 12pt]{article}\r\n\\usepackage[utf8]{inputenc}\r\n\\usepackage[slovak]{babel}\r\n\\usepackage{graphicx}\r\n\\usepackage{amsmath,amssymb,amsfonts}\r\n\r\n\\newenvironment{task}{}{}\r\n\\newenvironment{solution}{\\noindent\\textbf{Riešenie:}}{}\r\n\r\n\r\n\\begin{document}\r\n\r\n\r\n\\section*{O23A7A}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{6}{(5s+2)^2}e^{-4s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{2}-\\dfrac{3}{2}e^{-\\frac{2}{5}(t-4)}-\\dfrac{3}{5}(t-4)e^{-\\frac{2}{5}(t-4)} \\right] \\eta(t-4)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%===============================================================\r\n\r\n\r\n\\section*{O5A67B}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{35}{(2s+5)^2}e^{-6s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{7}{5}-\\dfrac{7}{5}e^{-\\frac{5}{2}(t-6)}-\\dfrac{7}{2}(t-6)e^{-\\frac{5}{2}(t-6)} \\right] \\eta(t-6)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%=============================================================\r\n\r\n\\section*{OAC346}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{12}{(5s+4)^2}e^{-7s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{4}-\\dfrac{3}{4}e^{-\\frac{4}{5}(t-7)}-\\dfrac{3}{5}(t-7)e^{-\\frac{4}{5}(t-7)} \\right] \\eta(t-7)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n\r\n\r\n\\end{document}\r\n', 485, NULL, NULL, 'aaaas'),
+(2, '2023-05-18 09:23:02', '\\documentclass[a4paper, 12pt]{article}\r\n\\usepackage[utf8]{inputenc}\r\n\\usepackage[slovak]{babel}\r\n\\usepackage{graphicx}\r\n\\usepackage{amsmath,amssymb,amsfonts}\r\n\r\n\\newenvironment{task}{}{}\r\n\\newenvironment{solution}{\\noindent\\textbf{Riešenie:}}{}\r\n\r\n\r\n\\begin{document}\r\n\r\n\r\n\\section*{O23A7A}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{6}{(5s+2)^2}e^{-4s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{2}-\\dfrac{3}{2}e^{-\\frac{2}{5}(t-4)}-\\dfrac{3}{5}(t-4)e^{-\\frac{2}{5}(t-4)} \\right] \\eta(t-4)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%===============================================================\r\n\r\n\r\n\\section*{O5A67B}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{35}{(2s+5)^2}e^{-6s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{7}{5}-\\dfrac{7}{5}e^{-\\frac{5}{2}(t-6)}-\\dfrac{7}{2}(t-6)e^{-\\frac{5}{2}(t-6)} \\right] \\eta(t-6)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%=============================================================\r\n\r\n\\section*{OAC346}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{12}{(5s+4)^2}e^{-7s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{4}-\\dfrac{3}{4}e^{-\\frac{4}{5}(t-7)}-\\dfrac{3}{5}(t-7)e^{-\\frac{4}{5}(t-7)} \\right] \\eta(t-7)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n\r\n\r\n\\end{document}\r\n', 485, NULL, NULL, 'aaaas'),
+(3, '2023-05-18 09:23:14', '\\documentclass[a4paper, 12pt]{article}\r\n\\usepackage[utf8]{inputenc}\r\n\\usepackage[slovak]{babel}\r\n\\usepackage{graphicx}\r\n\\usepackage{amsmath,amssymb,amsfonts}\r\n\r\n\\newenvironment{task}{}{}\r\n\\newenvironment{solution}{\\noindent\\textbf{Riešenie:}}{}\r\n\r\n\r\n\\begin{document}\r\n\r\n\r\n\\section*{O23A7A}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{6}{(5s+2)^2}e^{-4s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{2}-\\dfrac{3}{2}e^{-\\frac{2}{5}(t-4)}-\\dfrac{3}{5}(t-4)e^{-\\frac{2}{5}(t-4)} \\right] \\eta(t-4)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%===============================================================\r\n\r\n\r\n\\section*{O5A67B}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{35}{(2s+5)^2}e^{-6s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{7}{5}-\\dfrac{7}{5}e^{-\\frac{5}{2}(t-6)}-\\dfrac{7}{2}(t-6)e^{-\\frac{5}{2}(t-6)} \\right] \\eta(t-6)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%=============================================================\r\n\r\n\\section*{OAC346}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{12}{(5s+4)^2}e^{-7s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{4}-\\dfrac{3}{4}e^{-\\frac{4}{5}(t-7)}-\\dfrac{3}{5}(t-7)e^{-\\frac{4}{5}(t-7)} \\right] \\eta(t-7)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n\r\n\r\n\\end{document}\r\n', 485, NULL, NULL, 'aaaas'),
+(4, '2023-05-18 09:24:12', '\\documentclass[a4paper, 12pt]{article}\r\n\\usepackage[utf8]{inputenc}\r\n\\usepackage[slovak]{babel}\r\n\\usepackage{graphicx}\r\n\\usepackage{amsmath,amssymb,amsfonts}\r\n\r\n\\newenvironment{task}{}{}\r\n\\newenvironment{solution}{\\noindent\\textbf{Riešenie:}}{}\r\n\r\n\r\n\\begin{document}\r\n\r\n\r\n\\section*{O23A7A}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{6}{(5s+2)^2}e^{-4s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{2}-\\dfrac{3}{2}e^{-\\frac{2}{5}(t-4)}-\\dfrac{3}{5}(t-4)e^{-\\frac{2}{5}(t-4)} \\right] \\eta(t-4)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%===============================================================\r\n\r\n\r\n\\section*{O5A67B}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{35}{(2s+5)^2}e^{-6s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{7}{5}-\\dfrac{7}{5}e^{-\\frac{5}{2}(t-6)}-\\dfrac{7}{2}(t-6)e^{-\\frac{5}{2}(t-6)} \\right] \\eta(t-6)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%=============================================================\r\n\r\n\\section*{OAC346}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{12}{(5s+4)^2}e^{-7s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{4}-\\dfrac{3}{4}e^{-\\frac{4}{5}(t-7)}-\\dfrac{3}{5}(t-7)e^{-\\frac{4}{5}(t-7)} \\right] \\eta(t-7)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n\r\n\r\n\\end{document}\r\n', 485, NULL, NULL, 'aaaas'),
+(5, '2023-05-18 09:49:48', '\\documentclass[a4paper, 12pt]{article}\r\n\\usepackage[utf8]{inputenc}\r\n\\usepackage[slovak]{babel}\r\n\\usepackage{graphicx}\r\n\\usepackage{amsmath,amssymb,amsfonts}\r\n\r\n\\newenvironment{task}{}{}\r\n\\newenvironment{solution}{\\noindent\\textbf{Riešenie:}}{}\r\n\r\n\r\n\\begin{document}\r\n\r\n\r\n\\section*{O23A7A}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{6}{(5s+2)^2}e^{-4s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{2}-\\dfrac{3}{2}e^{-\\frac{2}{5}(t-4)}-\\dfrac{3}{5}(t-4)e^{-\\frac{2}{5}(t-4)} \\right] \\eta(t-4)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%===============================================================\r\n\r\n\r\n\\section*{O5A67B}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{35}{(2s+5)^2}e^{-6s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{7}{5}-\\dfrac{7}{5}e^{-\\frac{5}{2}(t-6)}-\\dfrac{7}{2}(t-6)e^{-\\frac{5}{2}(t-6)} \\right] \\eta(t-6)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n%=============================================================\r\n\r\n\\section*{OAC346}\r\n\\begin{task}\r\n    Vypočítajte prechodovú funkciu pre systém opísaný prenosovou funkciou\r\n    \\begin{equation*}\r\n        F(s)=\\dfrac{12}{(5s+4)^2}e^{-7s}\r\n    \\end{equation*}\r\n\\end{task} \r\n\r\n\\begin{solution}\r\n    \\begin{equation*}\r\n        y(t)=\\left[ \\dfrac{3}{4}-\\dfrac{3}{4}e^{-\\frac{4}{5}(t-7)}-\\dfrac{3}{5}(t-7)e^{-\\frac{4}{5}(t-7)} \\right] \\eta(t-7)\r\n    \\end{equation*}\r\n\\end{solution}\r\n\r\n\r\n\r\n\\end{document}\r\n', 485, NULL, NULL, 'aaaas');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -122,8 +133,7 @@ ALTER TABLE `Available_task_sets`
 -- Indexes for table `Images`
 --
 ALTER TABLE `Images`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `task_id` (`task_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `Students`
@@ -145,7 +155,8 @@ ALTER TABLE `Student_task_sets`
 --
 ALTER TABLE `Tasks`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `task_set_id` (`task_set_id`);
+  ADD KEY `task_set_id` (`task_set_id`),
+  ADD KEY `task_image_id` (`task_image_id`);
 
 --
 -- Indexes for table `Tasks_sets`
@@ -191,7 +202,7 @@ ALTER TABLE `Tasks`
 -- AUTO_INCREMENT for table `Tasks_sets`
 --
 ALTER TABLE `Tasks_sets`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -204,12 +215,6 @@ ALTER TABLE `Available_task_sets`
   ADD CONSTRAINT `Available_task_sets_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `Tasks_sets` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Constraints for table `Images`
---
-ALTER TABLE `Images`
-  ADD CONSTRAINT `Images_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `Tasks` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
 -- Constraints for table `Student_task_sets`
 --
 ALTER TABLE `Student_task_sets`
@@ -220,7 +225,8 @@ ALTER TABLE `Student_task_sets`
 -- Constraints for table `Tasks`
 --
 ALTER TABLE `Tasks`
-  ADD CONSTRAINT `Tasks_ibfk_1` FOREIGN KEY (`task_set_id`) REFERENCES `Tasks_sets` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `Tasks_ibfk_1` FOREIGN KEY (`task_set_id`) REFERENCES `Tasks_sets` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `Tasks_ibfk_2` FOREIGN KEY (`task_image_id`) REFERENCES `Images` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
